af5e5f86c25c48cbf37933aa73e1c4d2aacfc592
burlingate <burlingate1@gmail.com>
2024-02-22 00:31:45 -0800

    delete cookie

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+
+window.onload = function() {
+    // 모든 쿠키를 가져와서 배열로 변환
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        // 쿠키 삭제
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+    }
+};
